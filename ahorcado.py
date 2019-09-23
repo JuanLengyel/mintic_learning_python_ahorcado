52d8ab5a69e9328962fb17641286c2ebc70fb9a0
Juan Sebastian Lengyel <julengyel@personalsoft.com.co>
2019-09-23 18:27:53 -0500

    Finished v1 for hanged game in python console

--- ahorcado.py
+++ ahorcado.py
@@ -1,8 +1,10 @@
+import random
+
 ASCII_ART = [
     '''           
       ==========
-      ||      ||
-      ||      ||
+      |       ||
+      |       ||
               ||
               ||
               ||
@@ -15,9 +17,9 @@ ASCII_ART = [
     ''',
     '''
       ==========
-      ||      ||
-      ||      ||
-       O      ||
+      |       ||
+      |       ||
+      O       ||
               ||
               ||
               ||
@@ -29,11 +31,11 @@ ASCII_ART = [
     ''',
     '''
       ==========
-      ||      ||
-      ||      ||
-       O      ||
-       |      ||
-       |      ||
+      |       ||
+      |       ||
+      O       ||
+      |       ||
+      |       ||
               ||
               ||
               ||
@@ -43,11 +45,11 @@ ASCII_ART = [
     ''',
     '''
       ==========
-      ||      ||
-      ||      ||
-       O      ||
-      /|      ||
-     / |      ||
+      |       ||
+      |       ||
+      O       ||
+     /|       ||
+    / |       ||
               ||
               ||
               ||
@@ -57,11 +59,11 @@ ASCII_ART = [
     ''',
     '''
       ==========
-      ||      ||
-      ||      ||
-       O      ||
-      /|\     ||
-     / | \    ||
+      |       ||
+      |       ||
+      O       ||
+     /|\      ||
+    / | \     ||
               ||
               ||
               ||
@@ -71,13 +73,13 @@ ASCII_ART = [
     ''',
     '''
       ==========
-      ||      ||
-      ||      ||
-       O      ||
-      /|\     ||
-     / | \    ||
-      /       ||
+      |       ||
+      |       ||
+      O       ||
+     /|\      ||
+    / | \     ||
      /        ||
+    /         ||
               ||
               ||
       ----------
@@ -85,13 +87,13 @@ ASCII_ART = [
     ''',
         '''
       ==========
-      ||      ||
-      ||      ||
-       O      ||
-      /|\     ||
-     / | \    ||
-      / \     ||
-     /   \    ||
+      |       ||
+      |       ||
+      O       ||
+     /|\      ||
+    / | \     ||
+     / \      ||
+    /   \     ||
               ||
               ||
       ----------
@@ -99,8 +101,74 @@ ASCII_ART = [
     '''
 ]
 
+WORDS = [
+    'CACHETES',
+    'ZARZAMORA',
+    'JUEGO',
+    'RUIN',
+    'CIVILIZACION',
+    'ACERRIMO',
+    'CHIMPANCE',
+    'ALOCADO',
+    'MUESTRA'
+]
+
+MAX_TRY = len(ASCII_ART)
+
+def getRandomWord():
+    return WORDS[random.randint(0, len(WORDS) - 1)]
+
+def refreshPlayingBoard(tries, guess):
+    print(ASCII_ART[tries])
+    print('**---' + str.join(' ', guess) + '---**')
+
+def checkLetterCoincidences(wordToGuess, currentLetterGuess):
+    coincidenceIndexes = []
+    
+    for i in range(0, len(wordToGuess)):
+        if currentLetterGuess[0] == wordToGuess[i]:
+            coincidenceIndexes.append(i)
+
+    return coincidenceIndexes
+
+def checkWinCondition(currentGuessStatus):
+    currentGuessStatus.index("_")
+
+def checkGameOverCondition(currentTry):
+    return currentTry == MAX_TRY - 1
+
 def run():
-    pass
+    word = getRandomWord()
+    guess = ['_'] * len(word)
+
+    currentTry = 0
+
+    while True:
+        refreshPlayingBoard(currentTry, guess)
+        
+        currentLetterGuess = str(raw_input('Que letra te arriesgaras a adivinar: '))
+        currentLetterGuess = str.upper(currentLetterGuess)
+
+        coincidenceIndexes = checkLetterCoincidences(word, currentLetterGuess)
+
+        if 0 < len(coincidenceIndexes):
+            for i in coincidenceIndexes:
+                guess[i] = currentLetterGuess
+        else:
+            currentTry += 1
+
+        try:
+            checkWinCondition(guess)
+        except ValueError:
+            refreshPlayingBoard(currentTry, guess)
+            print('Te has salvado... Esta vez')
+            break
+
+        if(checkGameOverCondition(currentTry)):
+            refreshPlayingBoard(currentTry, word)
+            print('Tu sabiduria no fue suficiente >:)')
+            break
 
 if __name__ == "__main__":
+    print('Quieres jugar un juego?')
     run()
